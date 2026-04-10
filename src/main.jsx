98eb46ee47ff7800cbe09ec72204890cb1a18fdd
Steve Lane <sparemacbookpro13@MacBookPro.home>
2026-04-10 10:09:50 +0100

    remove index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css"; // optional: styling
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
